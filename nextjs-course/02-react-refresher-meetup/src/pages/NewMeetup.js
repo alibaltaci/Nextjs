c749b40f4bb21e4921fb38d4e6475428add6f15d
alibaltaci <alibaltaci13@gmail.com>
2022-09-10 22:27:32 +0300

    firebase

--- nextjs-course/02-react-refresher-meetup/src/pages/NewMeetup.js
+++ nextjs-course/02-react-refresher-meetup/src/pages/NewMeetup.js
@@ -1,10 +1,32 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
 
 function NewMeetupPage() {
+
+  const navigate = useNavigate(); 
+
+  function addMeetupHandler( meetupData ){
+
+    fetch(
+        "https://react-meetup-464df-default-rtdb.firebaseio.com/meetups.json",
+        {
+          method: "POST",
+          body: JSON.stringify(meetupData),
+          headers:{
+            "Content-Type": "aplication/json"
+          }
+        }
+      ).then( () => {
+        navigate("/");
+        // document.location.href = '/';  //You don't need to import "react-router-dom"
+      } );
+
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={ addMeetupHandler }  />
     </section>
   )
 }
