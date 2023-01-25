d22cb86be6d06d541ae5852a8b3b40b982fbe368
alibaltaci <alibaltaci13@gmail.com>
2023-01-25 23:49:10 +0300

    getAllEvents

--- nextjs-course/06-project-time-page-pre-rendering-and-data-fetching/helpers/api-util.js
+++ nextjs-course/06-project-time-page-pre-rendering-and-data-fetching/helpers/api-util.js
@@ -1,6 +1,23 @@
-export async function getAllEvents() {}
+export async function getAllEvents() {
+    const res = await fetch("https://s6-events-data-default-rtdb.firebaseio.com/events.json");
+    const data = await res.json();
+
+    const events = [];
+
+    for( const key of data){
+        events.push({
+            id: key,
+            ...data[key]
+        })
+    }
+
+    return events;
+}
 
 export async function getFeaturedEvents() {
     return DUMMY_EVENTS.filter((event) => event.isFeatured);
   }
+
+getAllEvents();
+
   
