026a521e18ecea1dc5168ae0c8d877128d9a1079
alibaltaci <alibaltaci13@gmail.com>
2023-02-01 00:08:26 +0300

    image

--- nextjs-course/07-optimizing-nextjs-apps/components/event-detail/EventLogistics.js
+++ nextjs-course/07-optimizing-nextjs-apps/components/event-detail/EventLogistics.js
@@ -3,6 +3,8 @@ import DateIcon from "../icons/DateIcon";
 import AddressIcon from "../icons/AddressIcon";
 import classes from "./EventLogistics.module.css";
 
+import Image from "next/image"
+
 function EventLogistics( props ) {
 
     const { date, address, image, imageAlt } = props;
@@ -18,7 +20,8 @@ function EventLogistics( props ) {
   return (
     <section className={ classes.logistics } >
         <div className={ classes.image } >
-            <img src={`/${image}`} alt={ imageAlt } />
+            {/* <img src={`/${image}`} alt={ imageAlt } /> */}
+            <Image src={`/${image}`} alt={ imageAlt } width={400} height={400} />
         </div>
         <ul className={ classes.list } >
             <LogisticsItem icon={ DateIcon }>
