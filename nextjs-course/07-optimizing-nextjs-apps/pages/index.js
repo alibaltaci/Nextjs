9b405ebd76a7cc28e70cfb96fdfe471ddc50c4e7
alibaltaci <alibaltaci13@gmail.com>
2023-01-31 20:33:23 +0300

    Head

--- nextjs-course/07-optimizing-nextjs-apps/pages/index.js
+++ nextjs-course/07-optimizing-nextjs-apps/pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../helpers/api-util";
 
@@ -16,10 +18,15 @@ export async function getStaticProps(){
 
 export default function Home( {featuredEvents} ) {
 
-  console.log( featuredEvents);
-
   return (
     <div >
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList  items={ featuredEvents } />
     </div>
   )
