d22c2209b7885d3c222a8b6f5b574b5dac08a486
alibaltaci <alibaltaci13@gmail.com>
2023-02-01 19:32:21 +0300

    form

--- nextjs-course/08-adding-beckend-code-with-api-routes-fullstack-react/pages/index.js
+++ nextjs-course/08-adding-beckend-code-with-api-routes-fullstack-react/pages/index.js
@@ -1,7 +1,37 @@
+import { useRef } from "react";
+ 
 export default function Home() {
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHendler(e){
+
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    
+    
+
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onClick={ submitFormHendler } >
+        <div>
+          <label htmlFor="email" >Your Email Address</label><br />
+          <input type="email" id="email" ref={ emailInputRef } />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="feedback">Your Feedback</label><br />
+          <textarea id="feedback" rows="5" ref={ feedbackInputRef } />
+        </div>
+        <br /> 
+        <button>Send Feedback</button>
+      </form>
     </div>
   )
 }
