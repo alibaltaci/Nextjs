f6f475b60805be6430515b4effd06eedfa3126f6
alibaltaci <alibaltaci13@gmail.com>
2023-02-09 19:12:46 +0300

    running monodb queries from inside api routes

--- nextjs-course/09-project-time-api-routes/pages/api/newsletter.js
+++ nextjs-course/09-project-time-api-routes/pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function( req, res ){
+import { MongoClient } from 'mongodb'
+
+export default async function( req, res ){
 
     if( req.method === "POST" ){
         
@@ -9,7 +11,16 @@ export default function( req, res ){
             return;
         }
 
-        console.log( userEmail );
+        const client = await MongoClient.connect('mongodb+srv://ali:gOyn1RsYIbNvcW0t@cluster0.o5yxamt.mongodb.net/newsletter?retryWrites=true&w=majority')
+        // .then( client => {const db = client.db()})
+
+        const db = client.db() // client.db('newsletter') ---> yukarıdaki url 'de belirttiğimiz için burada tekrar tanımlamaya gerek yok.
+
+        await db.collection( 'email' ).insertOne( {email: userEmail, date: new Date().toDateString() } )
+
+        client.close() // bağlantıyı kesmek için
+
+        // console.log( userEmail );
 
         res.status(201).json({ message: "Signed Up!" })
     }
