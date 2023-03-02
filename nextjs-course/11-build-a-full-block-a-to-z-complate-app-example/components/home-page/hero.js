fcb421377e931b32190666a36972f29ae252aa8c
alibaltaci <alibaltaci13@gmail.com>
2023-03-02 16:39:42 +0300

    added loader for image

--- nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/components/home-page/hero.js
+++ nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/components/home-page/hero.js
@@ -2,11 +2,25 @@ import Image from 'next/image'
 import classes from './hero.module.css'
 
 export default function Hero(){
+
+    // Resim için URL dizesi döndürelim
+    const myLoader = ({ src, width, quality }) => {
+        return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`
+      }
+
+    // İşlemden önce URL: src="/_next/image?url=%2Fimages%2Fsite%2Fali_image.JPG&w=640&q=75"
+    
+    // işlemden sonra URL: "http://localhost:3000//images/site/ali_image.JPG?w=640&q=75"
     
     return(
         <section className={ classes.hero }>
             <div className={ classes.image } >
-                <Image src='/images/site/ali_image.JPG' alt='My image' height={300} width={300} />
+                <Image
+                    // loader={myLoader} 
+                    src='/images/site/ali_image.JPG' 
+                    alt='My image' 
+                    height={300} 
+                    width={300} />  
             </div>
             <h1>Hi, I'm Ali</h1>
             <p>
@@ -15,4 +29,4 @@ export default function Hero(){
 
         </section>
     )
-}
+}11
