9d0d3419a54f97f363283e36e0a1f7ae93fafd2f
alibaltaci <alibaltaci13@gmail.com>
2023-03-03 13:02:03 +0300

    featured posts

--- nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/components/home-page/featured-posts.js
+++ nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/components/home-page/featured-posts.js
@@ -1,3 +1,12 @@
+import PostsGrid from '../posts/posts-grid'
+import classes from './featured-posts.module.css'
+
 export default function FeaturedPosts(){
     
+    return(
+        <section className={ classes.latest } >
+            <h2>Featured Posts</h2>
+            <PostsGrid />
+        </section>
+    )
 }
