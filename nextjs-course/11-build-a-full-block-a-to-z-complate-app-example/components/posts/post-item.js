c28c61e397036d9c40c74b79749cdd973ce5120a
alibaltaci <alibaltaci13@gmail.com>
2023-03-03 14:04:46 +0300

    post grid and post item

--- nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/components/posts/post-item.js
+++ nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/components/posts/post-item.js
@@ -1,5 +1,33 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import classes from './post-item.module.css'
 
-export default function PostItem(){
+export default function PostItem( props ){
 
+    const { image, title, date, excerpt, slug } = props.post
+
+    const formattedDate = new Date(date).toLocaleDateString('en-US',{
+        day: 'numeric',
+        month: 'long', // 01 --> January
+        year: 'numeric'
+    })
+
+    const imagePath = `/images/posts/${slug}/${image}`
+
+    return(
+        <li className={ classes.post } >
+            <Link>
+                <a>
+                    <div>
+                        <Image src={ imagePath } alt={ title } width={300} height={200} />
+                    </div>
+                    <div className={ classes.content } >
+                        <h3>{ title }</h3>
+                        <time>{ formattedDate }</time>
+                        <p>{ excerpt }</p>
+                    </div>
+                </a>
+            </Link>
+        </li>
+    )
 }
