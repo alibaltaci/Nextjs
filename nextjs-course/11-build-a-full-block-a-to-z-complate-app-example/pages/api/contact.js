fb24850d3b4b9123d05b0d639c4ce0f4da1854c3
alibaltaci <alibaltaci13@gmail.com>
2023-03-09 22:11:39 +0300

    added API route

---
+++ nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/pages/api/contact.js
@@ -0,0 +1,25 @@
+export default function hendler(req, res){
+
+    if( req.method === 'POST' ){
+
+        const { email, name, message } = req.body
+
+        if( !email || !email.includes('@') || !mame || name.trim() === '' || !message || message.trim() === '' ){
+            
+            res.status(422).json( {message: 'Invalid Input.'} )
+            return;
+        }
+
+        const newMessage ={
+            email,
+            name,
+            message
+        }
+
+        console.log(newMessage)
+
+        res
+        .status(201) //success
+        .json( {message: 'Successfully stored message.', message: newMessage} ) 
+    }
+}
