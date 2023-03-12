df833e60932fcdf26743a1458c954e0c7c2d7152
alibaltaci <alibaltaci13@gmail.com>
2023-03-12 17:42:48 +0300

    added head component to contac page

--- nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/pages/contact/index.js
+++ nextjs-course/11-build-a-full-block-a-to-z-complate-app-example/pages/contact/index.js
@@ -1,8 +1,19 @@
+import { Fragment } from "react";
+import Head from 'next/head'
 import ContactForm from "../../components/contact/contact-form";
 
 export default function ContactPage(){
     
     return(
-        <ContactForm />
+        <Fragment>
+            <Head>
+                <title>Contact Me</title>
+                <meta 
+                    name='description' 
+                    content='Send me your messages!' // bu sayfa için çok önemli olmayabilir ama yine de ekliyoruz
+                />
+            </Head>
+            <ContactForm />
+        </Fragment>
     )
 }
