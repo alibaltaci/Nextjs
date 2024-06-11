7d3b4e5f06c439de20aa0c2b693f2504551e0cf0
ali baltaci <alibaltaci13@gmail.com>
2024-06-11 23:07:53 +0300

    middleware

--- nextjs-14/l-3-route-handlers-demo/src/app/profile/api/route.ts
+++ nextjs-14/l-3-route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export const GET = async() => {
-    return new Response("Profile API data")
+import { cookies, headers } from "next/headers"
+import { type NextRequest } from "next/server"
+
+export const GET = async( request:NextRequest) => {
+
+    const requestHeaders = new Headers( request.headers )
+
+    const headerList = headers()
+
+    cookies().set("resultPerPage", "20")
+    const theme = request.cookies.get("theme")
+
+    console.log( requestHeaders.get("Authorization") )
+    console.log( headerList.get("Authorization") )
+    console.log( theme ) 
+    console.log( cookies().get("resultPerPage") ) 
+    
+    return new Response("<h1>Profile API data</h1>", {
+        headers:{
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
+        }
+    })
 }
